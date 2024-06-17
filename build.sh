9290f7b51ced2a9b54a46386f9647b1cbc23d877
Denis Patrut <denisrpatrut@gmail.com>
2024-06-17 12:27:32 +0900

    Move GUI folder, to also add console frontend

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p build
-gcc -g -Og gui/main.c -o build/tex2sdf_gui -lSDL2 -lm
-
+gcc -g -Og frontend/gui/main.c -o build/tex2sdf_gui -lSDL2 -lm
+gcc -g -Og frontend/cui/main.c -o build/tex2sdf -lm
