52935574379a4376fd81dfd11668ae34b0e3e899
Denis Patrut <denisrpatrut@gmail.com>
2024-08-01 21:19:50 +0900

    UI: Add in basics for UI interation/input

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 mkdir -p build
-gcc -g -O3 -Wall -pedantic frontend/console/main.c -o build/tex2sdf -lm
+gcc -g -Og frontend/gui/main.c -o build/tex2sdf_gui -lSDL2 -lm
+#gcc -g -Og -Wall -pedantic frontend/console/main.c -o build/tex2sdf -lm
