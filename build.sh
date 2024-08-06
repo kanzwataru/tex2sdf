4a4fb3e03c0ff100e55906c6c101d03b61d86f8f
Denis Patrut <denisrpatrut@gmail.com>
2024-08-06 12:16:20 +0900

    Remove UI frontend code from master branch for now

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 mkdir -p build
-#gcc -g -Og frontend/gui/main.c -o build/tex2sdf_gui -lSDL2 -lm
-gcc -g -Og -Wall -pedantic frontend/console/main.c -o build/tex2sdf -lm
+gcc -g -O3 -Wall -pedantic frontend/console/main.c -o build/tex2sdf -lm
